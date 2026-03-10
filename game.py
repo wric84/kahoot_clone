e339fc8fef1e2f127beaac762a75064c91f64961
Wric84 <cwright88@live.com>
2026-03-10 18:52:53 -0400

    We added timer

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 import random
+import time
 
 print("Welcome to our kahoots!")
 # add a score 
@@ -17,10 +18,10 @@ print("Hello", name, "Let's start our game!")
 # if answer == "python":
 #     print("Correct!")
 #     # update score if answer is correct
-#     score = score + 100
-# else:
-#     print("Wrong answer")
+#     score
 
+# Create a list of four letters that can be associated with the correct answer i.e. A, B, C, D
+letters = ["A", "B", "C", "D"]
 # Lets create a dictionary object with all of our questions stored inside of it that we can access randomly and however many we want
 
 questions = {
@@ -29,17 +30,19 @@ questions = {
         # give list of option to choose from
         "options":["Python", "JS", "C++", "Ruby"],
         # from those options, which one is correct?
-        "answer":"python"
+        "answer":"A"
     },
     "What symbol is used to write a comment in python":{
         "options": ["/", "#", "&", "~"],
-        "answer": "#"
+        "answer": "B"
     },
     "What function is used to display text in our terminal?":{
         "options":["display", "touch", "write", "print"],
-        "answer":"print"
+        "answer":"D"
     }
 }
+
+
 
 # randomize the questions into a list
 question_list = list(questions.keys())
@@ -58,21 +61,35 @@ for question in question_list:
     # we need to define and then print each of our options
     options = questions[question]["options"]
 
-    for option in options:
-        print(option)
+    for letter, option in enumerate(options):
+        print(letters[letter] + ".", option)
 
-    answer = input("Your answer:").strip().lower()
-    if answer == questions[question]["answer"]:
-        print("\n Correct! Score +100")
+    start_time = time.time()
+    # print(start_time)
+
+    answer = input("Your answer:").strip().upper()
+
+    end_time = time.time()
+    # print(end_time)
+
+    time_taken = end_time - start_time
+    # print(time_taken)
+    print("You took", round(time_taken,2), "seconds")
+
+    if time_taken > 10:
+        print("Too slow! No points this round!")
+        print("Your score this round is:", score)
+    elif answer == questions[question]["answer"]:
+        print("\nCorrect! Score +100")
         # update score if answer is correct
         score = score + 1
     else:
         print("Wrong answer")
 
-print("\n Final Score:", score)
+print("\nFinal Score:", score)
 # Lets see what our percentage correct was!
 percentage = round((score / len(questions))*100)
-print("\n You got", percentage, "% correct!")
+print("\nYou got", percentage, "% correct!")
 # print(questions["What function is used to display text in our terminal?"])
 # print(questions["What language is this quiz written in?"])
 # print(questions["What symbol is used to write a comment in python"])
